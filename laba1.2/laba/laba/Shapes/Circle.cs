2e041d7d6e7ab730d0a7b49bb49f69b4a9f29d80
Andytemp <andy844551@gmail.com>
2020-10-19 15:48:13 +0300

    ye

--- laba1.2/laba/laba/Shapes/Circle.cs
+++ laba1.2/laba/laba/Shapes/Circle.cs
@@ -1,6 +1,7 @@
 namespace Shapes{
     class Circle:Ellipse{
-        public Circle(int startPointX,int startPointY, int endPointX, int endPointY){
+        public Circle(int startPointX, int startPointY, int endPointX, int endPointY)
+        {
             BaseRectangle = new Square(startPointX, startPointY, endPointX, endPointY);
         }
     }
